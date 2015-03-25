cf1801fef15784b76760609f5da652a4c129d7f3
Gabarron <Gabarron@MacBook-Pro-de-Gabarron.local>
2015-03-25 13:58:19 -0300

    merge teste

--- Teatro/ViewController.m
+++ Teatro/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 //oifbdhs jx u dicx ih ci hi 
+//oi gata
 }
 
 - (void)didReceiveMemoryWarning {
