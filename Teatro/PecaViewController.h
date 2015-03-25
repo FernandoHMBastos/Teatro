e05cb9d0d3b8e6cfb0c440cb706fafac4746d62e
Marco Gabarron <marco.gabarron@yahoo.com.br>
2015-03-25 16:40:51 -0300

    Inclusão do endereçamento na página de help e adicionado as TabBar's(Home, Peças e Help).

--- Teatro/PecaViewController.h
+++ Teatro/PecaViewController.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  PecaViewController.h
 //  Teatro
 //
-//  Created by Fernando H M Bastos on 3/25/15.
+//  Created by Gabarron on 25/03/15.
 //  Copyright (c) 2015 Fernando H M Bastos. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface PecaViewController : UIViewController
 
 @end
-
---
+++ Teatro/homeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  Teatro
+//
+//  Created by Gabarron on 25/03/15.
+//  Copyright (c) 2015 Fernando H M Bastos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
